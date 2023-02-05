6c7c741cac13f6613dbc37877847324749275c16
DESKTOP-1V9OQ1L\GIGABYTE <emma05127@gmail.com>
2023-02-05 20:29:46 +0900

    Add: Router 로직 수정중

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import './App.css';
-import { Route, Routes } from 'react-router-dom';
-import AuthPage from './pages/AuthPage';
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import AuthPage from "./pages/AuthPage";
 
 function App() {
+  //JWT 확인 로직 만들기
+  //JWT 인증 X -> /signup
+  //JWT 인증 O -> /todo
+  const JWT = true ;
 
   return (
-      <Routes>
-        <Route path='/' element={<AuthPage />}/>
-        <Route path='/signup' element={<AuthPage />}/>
-      </Routes>
+    <Routes>
+      <Route path="/" element={<AuthPage />} />
+      <Route path="/signup" element={<AuthPage />} />
+      {/* <Route path='/todo' element={<TodoPage />}/> */}
+    </Routes>
   );
 }
 
