203c0bdb5ec820cd4c3f38ba22b0d4b26a34a770
DESKTOP-1V9OQ1L\GIGABYTE <emma05127@gmail.com>
2023-02-07 22:23:11 +0900

    Add: 로그인 기능 구현중 > 서버 연결 이슈 해결해야 됨

--- src/components/auth/AuthForm.js
+++ src/components/auth/AuthForm.js
@@ -4,10 +4,6 @@ import palette from "../../lib/styles/palette";
 import Button from "../../components/common/Button";
 import { useState } from "react";
 
-//토큰 인증
-//만약 토큰이 없다면? -> login page.
-//login-register은 initial 에 Action으로 구분
-
 const AuthFormBlock = styled.div`
   h3 {
     margin: 0;
@@ -44,30 +40,26 @@ const Footer = styled.div`
   }
 `;
 
+const StyledButton = styled(Button)`
+  margin-top: 1rem;
+`;
+
 const textMap = {
-  login: "로그인",
-  register: "회원가입",
+  signup: "회원가입",
+  signin: "로그인",
 };
 
-// type : login, register 구분
+// type : signup, signup 구분
 // form: user name, password, passwordConfirm
 // onChange
 const AuthForm = ({ type, form, onChange, onSubmit }) => {
-  let [stateType, setStateType] = useState(type);
-  const text = textMap[stateType];
-
-  // 로그인-회원가입 handler
-  const changeMode = (e) => {
-    e.preventDefault();
-    setStateType("register");
-  };
-
-  console.log(text);
+  const text = textMap[type];
   return (
     <AuthFormBlock>
       <h3>{text}</h3>
       <form onSubmit={onSubmit}>
         <StyledInput
+          data-testid="email-input"
           autoComplete="email"
           name="email"
           placeholder="이메일"
@@ -75,6 +67,7 @@ const AuthForm = ({ type, form, onChange, onSubmit }) => {
           defaltvalue={form.email}
         />
         <StyledInput
+          data-testid="password-input"
           autoComplete="password"
           name="password"
           type="password"
@@ -82,7 +75,7 @@ const AuthForm = ({ type, form, onChange, onSubmit }) => {
           defaltvalue={form.password}
           onchange={onChange}
         />
-        {stateType === "register" && (
+        {type === "signup" && (
           <StyledInput
             autoComplete="new-password"
             name="passwordConfirm"
@@ -92,28 +85,23 @@ const AuthForm = ({ type, form, onChange, onSubmit }) => {
             defaltvalue={form.passwordConfirm}
           />
         )}
-        <Button>로그인</Button>
+        {/* 나중에 이부분 js로 수정해도 될듯... */}
+        {type === "signup" ? (
+          <StyledButton fullWidth data-testid="signup-button">
+            회원가입
+          </StyledButton>
+        ) : (
+          <StyledButton fullWidth data-testid="signin-button">
+            로그인
+          </StyledButton>
+        )}
       </form>
 
       <Footer>
-        {stateType === "login" ? (
-          <button
-            onClick={(e) => {
-              setStateType("register");
-            }}
-          >
-            {" "}
-            회원가입{" "}
-          </button>
+        {type === "signup" ? (
+          <Link to="/signin">로그인</Link>
         ) : (
-          <button
-            onClick={(e) => {
-              setStateType("login");
-            }}
-          >
-            {" "}
-            로그인{" "}
-          </button>
+          <Link to="/signup">회원가입</Link>
         )}
       </Footer>
     </AuthFormBlock>
