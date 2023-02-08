dbcbe6b68e7ff7f4c09dc7bcd550343f03d744cf
DESKTOP-1V9OQ1L\GIGABYTE <emma05127@gmail.com>
2023-02-08 17:27:16 +0900

    Add: 로그인 기능 구현

--- src/containers/auth/SigninForm.js
+++ src/containers/auth/SigninForm.js
@@ -1,47 +1,54 @@
 import AuthForm from "../../components/auth/AuthForm";
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { actionValid, changeField } from "../../modules/auth";
-import { signUpApi } from "../../lib/api/auth";
-import { signInApi } from './../../lib/api/auth';
+import { changeField } from "../../modules/auth";
+import { signInApi } from "./../../lib/api/auth";
 
 const SigninForm = () => {
   const dispatch = useDispatch();
 
-  const { form, auth, authError } = useSelector(({ authReducer }) => ({
+  const { form } = useSelector(({ authReducer }) => ({
     form: authReducer.singIn,
-    auth: authReducer.auth,
-    authError: authReducer.authError,
   }));
 
-  //처음부터 메시지 15
-  const [valid, setValid] = useState(true);
-  const [message, setMessage] = useState("");
+  // 여기 있는 action을 다 리듀서로 보내버리고싶은데 일단 나중에 구현
 
   const emailFn = new RegExp("@");
-  const passwordFn = new RegExp("(?=(w|d){8,})"); // 공백 제외 8자 이상
+  const passwordFn = new RegExp("(?=.{8,})"); // 공백 제외 8자 이상
 
-  const onBlur = (e) => {
-    const { value, name } = e.target;
+  const [emailIsValid, setEmailIsValid] = useState(false);
+  const [pwisValid, setPwIsValid] = useState(false);
+  const [emailMessage, setEmailMessage] = useState("");
+  const [pwMessage, setPwMessage] = useState("");
+  const [isValid, setIsValid] = useState(false);
 
-    if (value === "") {
-      setValid(false);
-      setMessage("빈칸을 다 채워 주세요.");
-    }
+  /**
+   * @param {String} name : tag의 name을 의미합니다. email, password
+   * @param {String} value : 사용자 input을 의미합니다.
+   */
+  useEffect(() => {
+    if (emailIsValid && pwisValid) setIsValid(true);
+  }, [emailIsValid, pwisValid]);
 
+  const onChange = (e) => {
+    const { name, value } = e.target;
     if (name === "email") {
-      if (emailFn.test(value)) setValid(true);
-      else {
-        setValid(false);
-        setMessage("이메일에는 @가 포함되어야 합니다.");
+      if (emailFn.test(value)) {
+        setEmailIsValid(true);
+        setEmailMessage("");
+      } else {
+        setEmailIsValid(false);
+        setEmailMessage("이메일에는 @를 포함해 작성해주세요");
       }
     }
 
     if (name === "password") {
-      if (passwordFn.test(value)) setValid(true);
-      else {
-        setValid(false);
-        setMessage("비밀번호는 (공백 제외) 8자 이상이어야 합니다. ");
+      if (passwordFn.test(value)) {
+        setPwIsValid(true);
+        setPwMessage("");
+      } else {
+        setPwIsValid(false);
+        setPwMessage("비밀번호는 8자 이상으로 작성해주세요.");
       }
     }
 
@@ -54,35 +61,24 @@ const SigninForm = () => {
     );
   };
 
-  // for Submit handler
+  // FORM Submit handler
   const onSubmit = (e) => {
     e.preventDefault();
     const { email, password } = form;
-    console.log(email, password);
-
-    signInApi(email, password)
-      .then((res) => {
-        localStorage.setItem("access_token", res.data.access_token);
-        alert("로그인 성공");
-      })
-      .catch((err) => {
-        alert(err);
-      })
-      .finally(()=>{
-        console.log('요청 완');
-      });
+    signInApi(email, password);
   };
 
   return (
     <AuthForm
       type="signin"
       form={form}
-      onBlur={onBlur}
+      onChange={onChange}
       onSubmit={onSubmit}
 
       //유효성 검사 관련 props
-      valid={valid}
-      message={message}
+      isValid={isValid}
+      emailMessage={emailMessage}
+      pwMessage={pwMessage}
     />
   );
 };
--- src/lib/api/auth.js
+++ src/lib/api/auth.js
@@ -1,7 +1,12 @@
 import client from "./client";
 
-export const signInApi = async (email, password) =>{
-  client.post("/auth/signin", { email, password })};
+export const signInApi = async (email, password) => {
+  try {
+    await client.post("/auth/signin", { email, password });
+  } catch(err) {
+    alert(err.response.data.message);
+  }
+};
 
 export const signUpApi = async (email, password) =>
   client.post("/auth/signup", { email, password });
